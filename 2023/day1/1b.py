cb7b1cbee83c8a1066b97a6d3540693a47f179cc
xdesign-mikesmith <mike.smith@xdesign.com>
2023-12-01 14:50:24 +0000

    tidy

--- 2023/day1/1b.py
+++ 2023/day1/1b.py
@@ -1,74 +1,49 @@
-calibLines = []
-line_calibrations = []
-calibrations = []
-calibrationTotal = 0
+numberDict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8",
+              "nine": "9"}
 
 
 def read_calibration():
     global lines, line
+    calibration_lines = []
     lines = open("inputfull.txt").readlines()
     for i, line in enumerate(lines):
         stripped_line = line.strip('\n')
-        calibLines.append(stripped_line)
+        calibration_lines.append(stripped_line)
+    return calibration_lines
 
 
-read_calibration()
-
-numberDict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8",
-              "nine": "9"}
-
-calibration = ""
-potentialNumber = ""
-finished = False
-for line in calibLines:
-    finished = False
-    for digit in line:
-        if not digit.isalpha():
-            calibration += digit
-            finished = True
-        else:
-            potentialNumber = potentialNumber + digit
-            if numberDict.get(potentialNumber):
-                calibration += numberDict.get(potentialNumber)
-                finished = True
-                potentialNumber = ""
-                break
-            else:
-                for i, digit in enumerate(potentialNumber):
-                    if numberDict.get(potentialNumber[i:len(potentialNumber)]):
-                        if numberDict.get(potentialNumber[i:len(potentialNumber)]):
-                            calibration += str(numberDict.get(potentialNumber[i:len(potentialNumber)]))
-                            potentialNumber = ""
-                            finished = True
-        if finished == True:
-            break
-
-    finished = False
-    for digit in line[::-1]:
-        if not digit.isalpha():
-            calibration += digit
-            finished = True
-        else:
-            potentialNumber = potentialNumber + digit
-            if numberDict.get(potentialNumber):
-                calibration += numberDict.get(potentialNumber)
-                finished = True
-                potentialNumber = ""
-                break
-            else:
-                for i, digit in enumerate(potentialNumber[::-1]):
-                    if numberDict.get(potentialNumber[i:len(potentialNumber)][::-1]):
-                        if numberDict.get(potentialNumber[i:len(potentialNumber)][::-1]):
-                            calibration += str(numberDict.get(potentialNumber[i:len(potentialNumber)][::-1]))
-                            potentialNumber = ""
-                            finished = True
-        if finished == True:
-            break
-
-    calibrations.append(calibration)
+def extract_calibration(direction, line):
     calibration = ""
+    potential_number = ""
+    finished = False
 
-for calibration in calibrations:
-    calibrationTotal = calibrationTotal + int(calibration)
+    for digit in line[::direction]:
+        if not digit.isalpha():
+            calibration += digit
+            finished = True
+        else:
+            potential_number = potential_number + digit
+            for i, digit in enumerate(potential_number[::direction]):
+                if numberDict.get(potential_number[i:len(potential_number)][::direction]):
+                    if numberDict.get(potential_number[i:len(potential_number)][::direction]):
+                        calibration += str(numberDict.get(potential_number[i:len(potential_number)][::direction]))
+                        potential_number = ""
+                        finished = True
+        if finished:
+            break
+    return calibration
+
+
+calibrations = []
+for line in read_calibration():
+    line_calibrations = extract_calibration(1, line)
+    line_calibrations = line_calibrations + extract_calibration(-1, line)
+
+    calibrations.append(line_calibrations)
+    line_calibration = ""
+
+calibrationTotal = 0
+for line_calibration in calibrations:
+    calibrationTotal = calibrationTotal + int(line_calibration)
 
 print("Calibration Total : %s" % calibrationTotal)
